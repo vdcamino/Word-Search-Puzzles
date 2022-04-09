f57b96f21b92152b932b2e800fa6c2175dad8e2b
Victor Da Camino <victordacamino@gmail.com>
2022-04-09 02:01:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
 Victor Da Camino
-Matrícula: 00275734
-03/04/2022
+MatrÃ­cula: 00275734
+09/04/2022
 
 Project description: X
 - X
@@ -14,6 +14,7 @@ Project description: X
 #include <stdlib.h>
 #include <time.h>
 #include "puzzle.h"
+#include "trie.h"
 
 int main() {
 
@@ -23,19 +24,24 @@ int main() {
     int retornoAux;
     int nbRowsPuzzle;
     int nbColumnsPuzzle;
+    char wordAux[50];   // Used to print all the words in the trie
+    int initialPositionWordStringAux = 0; // Used to print all the words in the trie
     char identificadorAux;
 
     Puzzle* puzzle;
     puzzle = createPuzzle();
 
-    printf("-----------------------------------------------------------\n");
-    printf("                                                           \n");
-    printf("         __   __   __     ___  ___     _     __  ___       \n");
-    printf(" |    | |  | |__| |  \    |    |      / \   |__| |    |   |\n");
-    printf(" | /\ | |  | | \  |   |   |--| |---  /---\  | \  |    |---|\n");
-    printf(" |/  \| |__| |  \ |__/    ___| |___ /     \ |  \ |___ |   |\n");
-    printf("                                                           \n");
-    printf("___________________________________________________________\n");
+    trieNode* trie;
+    trie = populateTrieFromDictionary();
+
+    printf("--------------------------------------------------------------------------------\n");
+    printf("                                                                                \n");
+    printf("            __    ___    __       ___     ___     _     ___   ____        \n");
+    printf(" ||    || ||  || ||__| ||  \\    ||      ||       / \\   ||__| ||    ||   ||\n");
+    printf(" || /\\ || ||  || || \\  ||   |   ||==||  ||===   /===\\  || \\  ||    ||===||\n");
+    printf(" ||/  \\|| ||__|| ||  \\ ||__/     ___||  ||___  /     \\ ||  \\ ||___ ||   ||\n");
+    printf("                                                                                \n");
+    printf("________________________________________________________________________________\n");
 
     printf("Let's define the dimensions of our word search puzzle!\n");
     printf("Number of rows: ");
@@ -43,23 +49,31 @@ int main() {
     printf("Number of columns: ");
     scanf("%d", &puzzle->nbColumns);
 
-    // Declare the 2D array we are going to use to store our puzzle 
-    // Alocate it dynamically because do know the size it will have
+    // Declare the 2D array we are going to use to store our puzzle
+    // Allocate it dynamically because we don't know the size it will have
     puzzle->_2D_Array = generateMatrix(puzzle->nbRows, puzzle->nbColumns);
 
     printf("Choose the type of game you want to play:\n");
-    printf("1. YOU create the word search puzzle and I solve it in 3s\n");
+    printf("1. YOU create the word search puzzle and I solve it\n");
     printf("2. I create a word search puzzle and YOU solve it\n");
     scanf("%d", &gameChoice);
 
     switch (gameChoice) {
-    case 1:
-        humanCreate_MachineSolve(puzzle);
-        break;
+        case 1:
+            populatePuzzleWithUserInput(puzzle);
+            printPuzzle(puzzle);
+            //SOLVE PUZZLE
+            // GIVE OPTIONS WITH COUNTER
+            // IF 2 PRESSDE -> VERIFY IF THE ANSWER REALLY IS IN THE PUZZLE
+            // IF 1 PRESSED -> PRINT ANSWERS
+            break;
 
-    case 2:
-        machineCreate_HumanSolve(puzzle);
-        break;
+        case 2:
+            populatePuzzleWithRandomCharacters(puzzle);
+            printf("Please find the 5 remaining words contained in this puzzle:\n");
+            printPuzzle(puzzle);
+            printAllWords(trie, wordAux, initialPositionWordStringAux);
+            break;
     }
 
     destroyPuzzle(puzzle);   // We shall free puzzle's memory since it was allocated dynamically
